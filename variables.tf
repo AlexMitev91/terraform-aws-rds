713cc26ae93e56c1dbb75e45efa521bb4a409a4f
Alex Mitev <alex.mitev@hashicorp.com>
2023-11-01 15:07:56 +0200

    Add more variables

--- variables.tf
+++ variables.tf
@@ -15,3 +15,15 @@ variable "db_encrypted" {
   description = "Encrypt the database storage"
   type = bool
 }
+
+variable "random1" {
+  description = "Testing dropdown"
+}
+
+variable "random2" {
+  description = "Testing dropdown"
+}
+
+variable "random3" {
+  description = "Testing dropdown"
+}
